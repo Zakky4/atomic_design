c181ea4df0169829e16e231bf3356203234dc72b
zakky <sadaoza@gmail.com>
2024-06-15 13:41:30 +0900

    Add Organisms

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import { PrimaryButton } from './components/atoms/button/PrimaryButton';
 import { SecondaryButton } from './components/atoms/button/SecondaryButton';
 import { SearchInput } from './components/molecules/SearchInput';
-import './App.css';
+import { UserCard } from './components/organisms/user/UserCard';
+import './style.css';
+
+const user = {
+  name: '山田太郎',
+  image: 'https://images.unsplash.com/photo-1583512603805-3cc6b41f3edb?q=80&w=2832&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  email: 'test@example.com',
+  tel: '090-1234-5678',
+  company: 'テスト株式会社',
+  web: 'https://google.com',
+};
 
 function App() {
   return (
@@ -10,6 +20,7 @@ function App() {
       <SecondaryButton>検索</SecondaryButton>
       <br />
       <SearchInput />
+      <UserCard user={user} />
     </div>
   );
 }
